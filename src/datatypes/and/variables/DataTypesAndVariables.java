7d79f792623e04c43036126c51530c0c7e213252
User <https://github.com/karno786/Sqlite_git_test>
2021-03-09 01:23:54 +0600

    02. Data Type and Variables - Android Development Course Basic To Advanced  .

---
+++ src/datatypes/and/variables/DataTypesAndVariables.java
@@ -0,0 +1,37 @@
+
+package datatypes.and.variables;
+
+
+public class DataTypesAndVariables {
+
+    public static void main(String[] args) {
+       
+        int num = 10;
+        float myFloatNum = 5.0f;
+        char myChar = 'A';
+        boolean myBol = true;
+        String myString = "Alamin Karno";
+        
+        System.out.println(num);
+        System.out.println(myBol);
+        System.out.println(myFloatNum);
+        System.out.println(myChar);
+        System.out.println(myString);
+        
+        
+        int num1 = 5;
+        int num2 = 10;
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        
+        int number1 = 5,number2 = 10;
+        
+        System.out.println("Multiplication of Two Number: "+ number1*number2);
+        System.out.println("Sumation of Two Number: "+ (number1 + number2));
+        
+        
+        
+    }
+    
+}
